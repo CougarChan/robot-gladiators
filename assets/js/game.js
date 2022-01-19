0212a35df5df5f6d5c1c25d2c51dc4ef5afeae7b
CougarChan <gohan0136@gmail.com>
2022-01-18 19:42:51 -0500

    2nd

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playername = window.prompt("What is your robots name?");
+// What is this?
+ console.log(playername);
+ console.log ("This logs a string, good for leaving yourself a message");
+ console.log (10+10);
+ console.log ("Our robot name is" + playername);
+
+function fight(){
+ window.alert("The fight has begun!");
+
+
+}
+// fight();
